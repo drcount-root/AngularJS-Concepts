294e82ac0df62e175013b33e985f5bf2f18765c9
drcount-root <about.subhamsahu@gmail.com>
2023-04-12 17:49:00 +0530

    One way & Two way covered

--- BM/Section-2/Section-2-App/src/app/one-component/one-component.component.ts
+++ BM/Section-2/Section-2-App/src/app/one-component/one-component.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-one-component',
   templateUrl: './one-component.component.html',
-  styleUrls: ['./one-component.component.scss']
+  styleUrls: ['./one-component.component.scss'],
 })
 export class OneComponentComponent {
+  // serverId: number = 10;
+  // serverStatus: string = 'Active';
 
+  allowNewServer: boolean = true;
+
+  servers: string[] = [];
+  serverName: string = '';
+
+  constructor() {
+    // setTimeout(() => {
+    //   this.allowNewServer = false;
+    //   console.log('Timer End!');
+    // }, 3000);
+  }
+
+  OnButtonClick() {
+
+    if(this.serverName.length == 0){
+      console.log("Please enter server name, can't added without name")
+    }
+
+    this.servers.push(this.serverName);
+    console.log('Clicked!');
+    this.serverName = '';
+  }
+
+  onInputChange(name: Event) {
+    this.serverName = (<HTMLInputElement>name.target).value;
+    console.log((<HTMLInputElement>name.target).value);
+  }
 }
